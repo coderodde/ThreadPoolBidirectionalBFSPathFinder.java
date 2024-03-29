8e28b80da60b9e1c6e48deeda04cc8ab0aecb265
Rodion Efremov <coderodd3@gmail.com>
2024-03-29 17:39:02 +0200

    Committing before pulling.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 open module coderodde.ThreadPoolBidirectionalBFSPathFinder {
     requires java.logging;
+    requires java.base;
     exports com.github.coderodde.graph.pathfinding.delayed;
     exports com.github.coderodde.graph.pathfinding.delayed.impl;
 }
